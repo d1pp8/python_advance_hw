9bb23c5ef37b5ab6563835ed4510273de0db8066
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-04-23 10:38:24 +0200

    SQLalchemy create 2 tables and create a relationship between them

---
+++ HW 3/main.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer, String, Numeric, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+# 1
+db_path = Path(__file__).parent / "my_db_hw3.db"
+
+engine = create_engine(f"sqlite:///{db_path}")
+
+Base = declarative_base()
+
+# 2
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# 4
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(255))
+
+    products = relationship("Product", back_populates="category")
+
+
+# 3
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    price = Column(Numeric(10,2), nullable=False)
+    in_stock = Column(Boolean, default=True)
+
+    # 5
+    category_id = Column(Integer, ForeignKey("categories.id"))
+    category = relationship("Category", back_populates="products")
+
+
+Base.metadata.create_all(engine)
